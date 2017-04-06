0c17adf9d2881fe35e9e1512f6008967baaa1177
Shi Peng <spone.2014@gmail.com>
2017-04-06 18:36:13 +0800

    output parser

--- parserEval.py
+++ parserEval.py
@@ -12,13 +12,13 @@ def parserEval(source, gold):
   uas = float(parserString[20])
   return uas, las
 
-def embedding_output(save, sents, hidden_repre):
-  with open(save+"/parsing_bed",'a') as f:
+def embedding_output(save, filename, sents, hidden_repre):
+  with open(save+"/parsing_bed+filename",'a') as f:
     for sent, hidden in zip(sents, hidden_repre):
       for word, hid in zip(sent, hidden[1:]):
         f.write(word+" ")
         for item in hid:
-          f.write(item+" ")
+          f.write(str(item)+" ")
         f.write("\n")
 
 
