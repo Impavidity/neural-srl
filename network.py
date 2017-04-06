ad8b7a8ed5889dc203b0e73262b0d5423efa6000
Shi Peng <spone.2014@gmail.com>
2017-04-07 02:36:17 +0800

    random seed

--- network.py
+++ network.py
@@ -52,8 +52,8 @@ if __name__ == '__main__':
 
   # graph = tf.Graph()
   #with graph.as_default():
-  tf.set_random_seed(199501)
-  np.random.seed(199501)
+  tf.set_random_seed(19950105)
+  np.random.seed(19950105)
   #print(tf.get_default_graph().seed)
   if cargs['model_type'] == "SimpleSrler":
     cargs.pop("model_type","")
